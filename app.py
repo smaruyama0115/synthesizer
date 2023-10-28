8d83296ca75e92bbd476c30cdbf42e45b44421a2
Shohei Maruyama <s.maruyama0115@gmail.com>
2023-10-29 07:18:40 +0900

    fix host

--- app.py
+++ app.py
@@ -326,4 +326,4 @@ def sync_checklists(category_selected, all_selected, dropdown):
     return category_selected, all_selected, fig
 
 port = int(os.environ.get("PORT", 5000))
-app.run_server(debug=True, host="0.0.0.0", port=port)
+app.run_server(debug=True, port=port)
