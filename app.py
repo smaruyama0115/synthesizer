f557fea6c8727beb334b51f1f9c0527b47a50b10
Shohei Maruyama <s.maruyama0115@gmail.com>
2023-10-29 12:21:31 +0900

    debug

--- app.py
+++ app.py
@@ -333,4 +333,4 @@ def sync_checklists(category_selected, all_selected, dropdown):
     return category_selected, all_selected, fig
 
 if __name__=='__main__':
-    app.run_server(debug=False, port = 1256)
+    app.run_server(debug=False)
